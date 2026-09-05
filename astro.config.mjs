6d2c558404aa8d5712203fea6ab3bd601f6939a6
Hazel <325257037+hzllim@users.noreply.github.com>
2026-09-05 22:02:42 +0800

    fix: astro and vercel versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,17 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import keystatic from "@keystatic/astro";
 import markdoc from "@astrojs/markdoc";
+import vercel from "@astrojs/vercel";
 
 const isDev = process.env.NODE_ENV !== "production";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel(),
+  site: process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:4321",
   base: "/",
   integrations: [react(), markdoc(), keystatic()],
   redirects: {
